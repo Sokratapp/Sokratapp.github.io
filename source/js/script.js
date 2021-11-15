854fa3c7a75bf772a7fa6d76409528230fed9bfd
Anna <korobovaanna89@gmail.com>
2021-11-15 15:19:43 +0300

    Попап категории

--- source/js/script.js
+++ source/js/script.js
@@ -4,3 +4,14 @@ var swiper = new Swiper(".mySwiper", {
     el: ".swiper-pagination",
   },
 });
+
+const links = document.querySelectorAll('.modals__item');
+
+for (let i = 0; i < links.length; i++) {
+  links[i].addEventListener("click", function (evt) {
+    evt.preventDefault();
+    const currentLink = document.querySelector(".modals__item .active");
+    links[i].classList.toggle("active");
+  });
+};
+
