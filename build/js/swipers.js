165adac8818d22c4f1618019f9581aef8b68b581
Artemii-Belousov <neochoto@mail.ru>
2022-01-10 23:06:15 +0500

    правки 10.01.2022

--- build/js/swipers.js
+++ build/js/swipers.js
@@ -12,7 +12,7 @@ let mainPageSwiper = new Swiper(".main-page-swiper__swiper", {
     768: {
       spaceBetween: 20,
     }
-  }
+  },
 });
 
 let mainPagePopularSwiper = new Swiper(".block-popular__swiper", {
@@ -45,5 +45,51 @@ let mainPageCompilationsSwiper = new Swiper(".compilations__swiper", {
     768: {
       spaceBetween: 21,
     }
+  },
+  navigtion: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+});
+
+let swiperTeacher = new Swiper('.teacher__list',{
+  slidesPerView: 5,
+  // loop: true,
+  centeredSlides: false,
+  spaceBetween: 20,
+  breakpoints: {
+    768: {
+      spaceBetween: 19,
+    }
   }
 });
+ 
+let courseProgrammList  = new Swiper('.course__programm-list ', {
+  direction: 'horizontal',
+  slidesPerView: 2.5,
+  slidesPerColumn: 3,
+  spaceBetween: 20,
+  slidesPerColumnFill: 'row',
+  // autoHeight: true,
+ 
+
+  // Управление колесом мыши
+  mousewheel: {
+    sensitiviti: 1,
+  }
+  
+});
+
+let skillsList = new Swiper('.skills__list',{
+  direction: 'horizontal',
+  slidesPerView: 3,
+  slidesPerColumn: 3,
+  spaceBetween: 15,
+  slidesPerColumnFill: 'row',
+
+  mousewheel: {
+    sensitiviti: 1,
+  }
+});
+
+
